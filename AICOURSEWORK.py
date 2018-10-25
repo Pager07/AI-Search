b790c30eef0dc21c0b97d11d1032735f3df9d391
Sandeep <sandeep@er1-10-245-8-103.wlan.dur.ac.uk>
2018-10-25 14:24:36 +0100

    Added new methods

--- AICOURSEWORK.py
+++ AICOURSEWORK.py
@@ -1,4 +1,6 @@
 import numpy as np
+import networkx as nx
+
 file_path = "/Users/sandeep/Desktop/Year 2/SoftwareMethod/AI-Search/NEWAISearchfile012.txt"
 
 f = open(file_path, "r")
@@ -11,8 +13,19 @@ raw_text = [line for line in f.readlines() if line.strip()]
 raw_text = [string.rstrip() for string in raw_text] 
 
 
+def get_num_cities():
+    unprocessed_size_string = raw_text[1]
+    size_string = ""
+    for letter in unprocessed_size_string:
+        if letter.isdigit():
+            size_string += letter
+    
+    num_cities = int(size_string)
+    return num_cities
+    
 
 #make a function that makes the matrix
+#this is your f(x)
 def distance_matrix():
     #getting the num of cities
     unprocessed_size_string = raw_text[1]
@@ -48,22 +61,86 @@ def distance_matrix():
                 mat[x,y] = mat[y,x]          
     return mat
 
+mat = distance_matrix()
+G = nx.Graph()
+def initial_state_node(start_city):
+    visited_list = [start_city]
+    pc = 0
+    min_distance_option = []
+    city_distances = mat[start_city] 
+    for city , distance in enumerate(city_distances):
+        if city not in visited_list:
+            min_distance_option.append(city_distances[city])
+    print(min_distance_option)
+    h_value = min(min_distance_option)
+    feed_dict = {'h': h_value,
+                 'pc': pc,
+                 'visited':visited_list
+                }
+    G.add_node(0 ,state_info = feed_dict)
+    print(h_value , pc , visited_list)
+
+
+initial_state_node(0)
+    
+    
+        
+        
+            
+            
+            
+            
+    
+        
+        
+    
+    
+    
+    
+def a_star_search(start_city):
+    #
+    minimum_history=[]
+    
+    
+
+
 
 ##finding heuristice values for each node
 #h(x) = distacne to next node(smallest)
+#
+#def heuristic_values(mat, visited_cities):
+#    #i want to get min value for each city unvisted city/row
+#    #i dont know which cities are viested 
+#    #but there was zero, becuase there was distacne connected to itslef
+#    #so i said if, where valvue is greater than 5, do nothing,elese replace it with max value
+#    #then just pick the min
+#    #example [0,1,4,6]->[6,1,2,6] and now pick miniimun
+#    #here I removed the zeros
+#    mat = np.where(mat>0, mat,mat.max())
+#    #i need to know where the minimum value came from
+#    #[6,6,2,6]
+#    #[2,1,3,5]
+#    #may be i could replace the value with max (x,ycurrent) every time i pick a min
+#    #so for (1,5) i will go to (1,5) and replace it with max value
+#    #so it wound be choosen
+#    rows = get_num_cities()
+#    cols = get_num_cities()
+#    h_values = []
+#    print(rows, cols)
+#    current_min = mat.max()
+#    for  row in range(rows):
+#        for col in range(cols):
+#            if mat[row][col] < current_min:
+#                current_min = mat[row][col]
+#        h_values.append(current_min)           
+#        mat[col][row] = mat.max()          
+#        current_min = mat.max()
+#    
+#    return h_values
 
-def heuristic_values(mat):
-    #i want to get min value for each city/row
-    #but there was zero, becuase there was distacne connected to itslef
-    #so i said if, where valvue is greater than 5, do nothing,elese replace it with max value
-    #then just pick the min
-    #example [0,1,4,6]->[6,1,2,6] and now pick miniimun
-    h_values = np.where(mat>0, mat,mat.max()).min(1)
-    return h_values
-
-
-
-
+    
+    
+    
 
 
 
